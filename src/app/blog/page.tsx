8514ad1092144e40a88942cd0aa5f0401476b4f4
faizan1699 <123071794+faizan1699@users.noreply.github.com>
2024-07-24 23:28:55 +0500

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ const blogPosts = [
     id: 1,
     imgSrc: "/imgs/blog/1.jpg",
     altText: "Responsive Web Design",
-    date: "June 4, 2020",
+    date: "June 4, 2024",
     title: "Mastering Responsive Web Design",
     description:
       "In today's digital landscape, responsive web design is not just a trend; it is a necessity. Explore the principles and techniques behind creating websites that adapt seamlessly to various devices and screen sizes.",
@@ -15,7 +15,7 @@ const blogPosts = [
     id: 2,
     imgSrc: "/imgs/blog/2.jpg",
     altText: "Creative Slideshow",
-    date: "June 4, 2020",
+    date: "June 4, 2024",
     title: "Unleashing Creativity with Slideshows",
     description:
       "Discover innovative ways to create captivating slideshows that engage your audience. From stunning visuals to seamless transitions, this blog post explores the art of crafting memorable slideshows for your website.",
@@ -25,7 +25,7 @@ const blogPosts = [
     id: 3,
     imgSrc: "/imgs/blog/3.jpg",
     altText: "Image Gallery Lightbox",
-    date: "June 4, 2020",
+    date: "June 12, 2023",
     title: "Enhancing User Experience with Image Gallery Lightboxes",
     description:
       "Explore the benefits of incorporating image gallery lightboxes into your website design. From improved user experience to showcasing your visual content in style, this blog post sheds light on this valuable web design feature.",
@@ -35,7 +35,7 @@ const blogPosts = [
     id: 4,
     imgSrc: "/imgs/blog/4.jpeg",
     altText: "Node.js Basics",
-    date: "June 4, 2020",
+    date: "June 11, 2023",
     title: "Getting Started with Node.js",
     description:
       "Learn the fundamentals of Node.js and how it can be used to build scalable and efficient server-side applications.",
@@ -45,7 +45,7 @@ const blogPosts = [
     id: 5,
     imgSrc: "/imgs/blog/5.jpeg",
     altText: "Express.js Overview",
-    date: "June 4, 2020",
+    date: "June 3, 2024",
     title: "Express.js: A Minimalist Web Framework for Node.js",
     description:
       "Discover the power of Express.js and how it simplifies the process of building web applications with Node.js.",
@@ -55,7 +55,7 @@ const blogPosts = [
     id: 6,
     imgSrc: "/imgs/blog/6.jpeg",
     altText: "Next.js Introduction",
-    date: "June 4, 2020",
+    date: "Jan 1, 2024",
     title: "An Introduction to Next.js: React Framework for Production",
     description:
       "Explore Next.js, a powerful framework for building server-rendered React applications with ease and efficiency.",
@@ -65,7 +65,7 @@ const blogPosts = [
     id: 7,
     imgSrc: "/imgs/blog/7.png",
     altText: "React.js Best Practices",
-    date: "June 4, 2020",
+    date: "may 5, 2024",
     title: "Best Practices for Developing with React.js",
     description:
       "Learn essential tips and techniques for building scalable and maintainable applications with React.js.",
