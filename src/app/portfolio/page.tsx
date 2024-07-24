cab50d13a79c82ff63b0a8bb30e258fddebc15a1
faizan1699 <faizanrasheed169@gmail.com>
2024-07-24 18:58:11 +0500

    some fixing

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,27 +5,13 @@ const Portfolio: NextPage = () => {
     <div>
       <section className="portfolio section" id="portfolio">
         <div className="container">
+          
           <div className="row">
             <div className="section-title padd-15">
               <h2>Portfolio</h2>
             </div>
           </div>
-          <div className="row">
-            <div className="portfolio-filter padd-15">
-              <button type="button" className="active" data-filter="all">
-                All
-              </button>
-              <button type="button" data-filter="graphics-design">
-                Graphics Design
-              </button>
-              <button type="button" data-filter="web-design">
-                Web Design
-              </button>
-              <button type="button" data-filter="wordpress">
-                Wordpress
-              </button>
-            </div>
-          </div>
+         
           <div className="row">
             {/* Portfolio Item */}
             <div className="portfolio-item padd-15" data-category="web-design">
